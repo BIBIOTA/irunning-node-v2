fc9e40ea2efed76882cf4599f9199946ff0e81e0
Yuki Ota <yukiotataitien@gmail.com>
2023-07-07 13:48:00 +0800

    feat(env): Set default port by env

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,6 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.enableCors();
-  await app.listen(8080);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
